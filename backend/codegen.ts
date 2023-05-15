1fe2e0f82e67c734395903b9db34224980e8499d
nicolas l <nicolaslandazabal@hotmail.com>
2023-05-14 19:01:29 -0500

    little changes

--- backend/codegen.ts
+++ backend/codegen.ts
@@ -1,18 +1,4 @@
 import { CodegenConfig } from '@graphql-codegen/cli'
-import dotEnv from 'dotenv'
-// import { info } from './globals.js'
-// E.g SHOPIFY_STORE_URL={mystore}.myshopify.com
-
-// console.log('codegen', url, access_token)
-
-
-// const { url, access_token } = getCredentials()
-
-
-// const access_token = "b0150d3d45d54fdd5047c1b9beda010d"
-// const url = "https://9de8bb.myshopify.com/api/graphql.json"
-
-// dotEnv.config()
 
 const url = process.env.DATA_1 || "" as string
 const access_token = process.env.DATA_2 || "" as string
